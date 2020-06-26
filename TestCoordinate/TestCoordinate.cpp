574685cf92e6e5bd61f6084d1e17148536323d15
ByDiDinDiy <dmitriy0bydi@gmail.com>
2020-06-26 17:08:39 +0400

    Added Unit Test

---
+++ TestCoordinate/TestCoordinate.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CleanCode/Functions.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestCoordinate
+{
+	TEST_CLASS(TestCoordinate)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
