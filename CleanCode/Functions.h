0214098264162fd0e7f9011657e8d51ecaca3dbc
ByDiDinDiy <dmitriy0bydi@gmail.com>
2020-06-26 16:21:39 +0400

    Added prototype for function cmpCoordinate

--- CleanCode/Functions.h
+++ CleanCode/Functions.h
@@ -22,3 +22,13 @@ int deleteCommentsInCode(char text[MAX_SIZE_STRING][MAX_SIZE_SYMBOL], const int 
 *  return - true, если найдено или false, если не найдено
 */
 bool findStrInText(const char text[MAX_SIZE_STRING][MAX_SIZE_SYMBOL], const int lengthOfText, const char* symbolInput, int* x, int* y);
+
+/*!
+* Сравнение координат двух символов:Если первый символ ближе к началу, то функция возвращает 0, иначе 1.
+*  \param [in] x1 координата x для первой точки
+*  \param [in] y1 координата y для первой точки
+*  \param [in] x2 координата x для второй точки
+*  \param [in] y2 координата y для второй точки
+*  return - 0, eсли первая точка ближе к центру координат, 1 если наоборот
+*/
+bool cmpCoordinate(const int x1, const int y1, const int x2, const int y2);
